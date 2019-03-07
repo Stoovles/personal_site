0d18b5aa4eedf0a3ddcba05fcc467263348061ba
Ethan Grab <ethangrab@gmail.com>
2019-03-07 13:07:48 -0700

    Derp...added _test to file name

--- test/features/user_sees_an_error_page_test.rb
+++ test/features/user_sees_an_error_page_test.rb
@@ -1,8 +1,8 @@
 require './test/test_helper'
 
-class HomepageTest < CapybaraTestCase
-  def test_user_can_see_the_homepage
-    visit '/'
+class ErrorpageTest < CapybaraTestCase
+  def test_user_can_see_error_page
+    visit '/blog'
 
     assert page.has_content?("Page not found.")
     assert_equal 404, page.status_code
